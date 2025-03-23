c461940fb1ae7014ebd1fd1a8c26a7082f4c015d
Matin <i.e.matt.tavakoli@gmail.com>
2025-03-24 02:05:39 +0330

    step 2 final

--- src/db/Entity.java
+++ src/db/Entity.java
@@ -1,10 +1,19 @@
 package db;
 
-public abstract class Entity {
+public abstract class Entity implements Cloneable {
     static int count = 1;
     public int id;
 
     public Entity() {
         this.id = count++;
+    }
+
+    public Entity clone(){
+        try {
+            return (Entity) super.clone();
+        }
+        catch (CloneNotSupportedException e) {
+            throw new RuntimeException("Cloning not supported", e);
+        }
     }
 }
